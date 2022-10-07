8be1fcc1de0277bf930bcdf2ee7db0a243a8d9f6
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-07 20:46:09 +0200

    add fetching from mongoDB collection

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
-export default function Home() {
+import dbConnect from "../lib/dbConnect";
+import Question from "../models/Question";
+import QuestionCard from "../components/QuestionCard";
+
+export async function getServerSideProps() {
+  async function getAllQuestion() {
+    await dbConnect();
+    const questions = await Question.find();
+    const sanatizedQuestions = questions.map((question) => ({
+      id: question.id,
+      question: question.question,
+      answer: question.answer,
+      options: question.options,
+    }));
+    return sanatizedQuestions;
+  }
+
+  const questions = await getAllQuestion();
+  return {
+    props: { questions: questions },
+  };
+}
+
+export default function Home({ questions }) {
   return (
     <main>
       <h1>All questions</h1>
-      <ul></ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionCard
+            key={question.id}
+            question={question.question}
+            answer={question.answer}
+            options={question.options}
+          />
+        ))}
+      </ul>
     </main>
   );
 }
